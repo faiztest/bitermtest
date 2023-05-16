fa3017d57d23422153dd0e0bfa197ea341b6b0ad
faiztest <129721483+faiztest@users.noreply.github.com>
2023-05-16 10:29:57 +0700

    Update home.py

--- home.py
+++ home.py
@@ -112,14 +112,14 @@ if uploaded_file is not None:
         topic_abs = paper.Abstract_stop.values.tolist()
           
         @st.cache
-        def bitermdata(NUM):
+        def bitermdata():
              X, vocabulary, vocab_dict = btm.get_words_freqs(topic_abs)
              tf = np.array(X.sum(axis=0)).ravel()
              docs_vec = btm.get_vectorized_docs(topic_abs, vocabulary)
              docs_lens = list(map(len, docs_vec))
              biterms = btm.get_biterms(docs_vec)
              model = btm.BTM(
-                 X, vocabulary, seed=12321, T=NUM, M=20, alpha=50/8, beta=0.01)
+                 X, vocabulary, seed=12321, T=num_bitopic, M=20, alpha=50/8, beta=0.01)
              model.fit(biterms, iterations=20)
              p_zd = model.transform(docs_vec)
              coherence = model.coherence_
@@ -128,8 +128,8 @@ if uploaded_file is not None:
              phi = tmp.get_phi(model)
              return
                
-        num_bitopic = st.slider('Choose number of topics', min_value=4, max_value=20, step=1)
-        bitermdata(4)
+        num_bitopic = st.slider('Choose number of topics', min_value=8, max_value=20, step=1)
+        bitermdata()
         tab1, tab2 = st.tabs(["Viz", "Owl"])
         with tab1:
           num_bitopic_vis = st.selectbox(
