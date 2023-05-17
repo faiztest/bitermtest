98999a401ea313ad2573b85482d3ef7e2991b732
faiztest <129721483+faiztest@users.noreply.github.com>
2023-05-17 15:04:39 +0700

    Update home.py

--- home.py
+++ home.py
@@ -99,7 +99,7 @@ if uploaded_file is not None:
                     num_bitopic_vis = st.selectbox(
                          'Choose topic',
                          (totaltop))
-                    terms_probs = tmp.calc_terms_probs_ratio(phi, topic=num_bitopic_vis, lambda_=1)
+                    terms_probs = tmp.calc_terms_probs_ratio(phi, topic=num_bitopic_vis, lambda_=1, theme=None)
                     btmvis_probs = tmp.plot_terms(terms_probs)
                     st.altair_chart(btmvis_probs, use_container_width=True)
           
