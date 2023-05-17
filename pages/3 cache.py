6679ff3d39f471c6cbe8489103da926fe681362e
faiztest <129721483+faiztest@users.noreply.github.com>
2023-05-17 13:42:57 +0700

    Update 3 cache.py

--- pages/3 cache.py
+++ pages/3 cache.py
@@ -74,7 +74,7 @@ if uploaded_file is not None:
         
     #===Biterm===
     if method == 'Biterm':
-        num_bitopic = st.slider('Choose number of topics', min_value=8, max_value=20, step=1)
+        num_bitopic = st.slider('Choose number of topics', min_value=2, max_value=20, step=1)
         topic_abs = paper.Abstract_stop.values.tolist()       
         X, vocabulary, vocab_dict = btm.get_words_freqs(topic_abs)
         tf = np.array(X.sum(axis=0)).ravel()
