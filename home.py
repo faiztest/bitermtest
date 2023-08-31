8263e90d4ef3cfa8b16a6333d7439bbad07b02bd
faiztest <129721483+faiztest@users.noreply.github.com>
2023-08-31 07:44:58 +0700

    Update home.py

--- home.py
+++ home.py
@@ -51,6 +51,18 @@ hide_menu = """
      	</style>
      	"""
 st.markdown(hide_menu, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.subheader('Put your file here...')
 
